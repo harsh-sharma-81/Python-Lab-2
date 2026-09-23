976744cb2f5939aca18efc0da7bec9b1f9e094b4
Harsh <vasuharshsharma@gmail.com>
2026-09-23 12:15:05 +0530

    Added max program

---
+++ max.py
@@ -0,0 +1,9 @@
+def max(a,b):
+    if (a>b):
+        print("Maximum number among these two numbers is:",a)
+    else:
+        print("Maximum number among these two numbers is:",b)
+
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 2:"))
+max(a,b)
